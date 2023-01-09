9160a9fa64af4d1439142e12271d39072b68dfe9
Vijaya PORALLA <vijaya.poralla@niji.fr>
2023-01-09 14:32:52 +0100

    developed react login to show divided subsets in Board.jsx

--- 13cards/src/board/Board.jsx
+++ 13cards/src/board/Board.jsx
@@ -3,19 +3,39 @@ import { Cards } from '../cards/Cards';
 import './board.css'
 
 export const Board = () => {
-    // const [ar, setAr] = useState(['6♣', '12♠', '8♦', '10♣', '1♣', '3♥', '6♦', '9♣', '9♠', '13♠', '4♥', '12♥', '6♥']);
-    // const [ar1, setAr1] = useState(['6♦', '7♦', '8♦']);
-
-
-
+    // const [cart, setAr] = useState([
+    //     ['6♣', '12♠', '8♦', '10♣'],
+    //     ['9♣', '9♠', '13♠'],
+    //     ['1♣', '3♥', '6♦'],
+    //     ['2♣', '9♥', '7♦'],
+    // ]);
+    // const [cart, setAr1] = useState(['6♣', '12♠', '8♦', '10♣', '1♣', '3♥', '6♦', '9♣', '9♠', '13♠', '4♥', '12♥', '6♥']);
     return (
+        // <div className='test' style={{ display: 'flex' }} key={'fgb'}>
+        //     {cart.map((cards, index) => {
+        //         console.log(cart.length);
+        //         return (
+        //             <div key={index} style={{ display: 'flex' }}>
+        //                 {cart.length !== 13 ?
+        //                     <>
+        //                         {cards.map((subItems, sIndex) => {
+        //                             return (
+        //                                 <div className='card' key={sIndex}>
+        //                                     <h2>{subItems}</h2>
+        //                                 </div>)
+        //                         })}
+        //                         <div className="middleSpace"></div>
+        //                     </> :
+        //                     <div className='card' key={index}>
+        //                         <h2>{cards}</h2>
+        //                     </div>
+        //                 }
+        //             </div>
+        //         );
+        //     })}
+        // </div>
+
         <div className='board'>
-            {/* <div className='test' style={{ display: 'flex' }}> 
-             {ar.map((card, id) => {
-                return <div className='card' key={id}>
-                    <h2>{card}</h2>
-                </div>
-            })} */}
             <div>
                 <Cards />
             </div>
